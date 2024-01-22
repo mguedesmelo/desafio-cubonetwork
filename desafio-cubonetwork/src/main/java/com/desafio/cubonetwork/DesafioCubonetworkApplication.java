6beb72176a888add3e7fc10282e2836f97e846cd
m.guedes.de.melo <m.guedes.de.melo@WINAPzDF4JwHDrn.corp.avanade.org>
2024-01-22 10:49:55 -0800

    Initial version

---
+++ desafio-cubonetwork/src/main/java/com/desafio/cubonetwork/DesafioCubonetworkApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.cubonetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCubonetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCubonetworkApplication.class, args);
+	}
+
+}
